675fb3c1ec4d7aaa2724c3f937072b3fc061b760
munenebrian <njerubrianmunene@gmail.com>
2022-09-26 09:02:01 +0300

    commit tree b80f0d1e6a86f6b84ca9e176425e25f87276994e

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,14 +1,16 @@
 class StudentsController < ApplicationController
+
   def index
-    students = Students.all
+    students = Student.all
     render json: students
   end
+
   def grades
-    grades = Students.all.order("grade DESC")
+    grades = Student.all.order("grade DESC")
     render json: grades
   end
   def highest_grade
-    highest_grade = Students.all.order("grade DESC").first
+    highest_grade = Student.all.order("grade DESC").first
     render json: highest_grade
   end
 end
